eb06173fd13901bfb524847751f8b28c2ee5a0e3
cashmy <cmyers880@gmail.com>
2021-09-08 10:41:16 -0500

    Added Default styling elements of page header and grid layout

--- src/features/cohorts/CohortAssignment.jsx
+++ src/features/cohorts/CohortAssignment.jsx
@@ -1,9 +1,26 @@
 import React, { Fragment } from "react";
 import clsx from "clsx";
 
-import { Grid, Typography, makeStyles } from "@material-ui/core";
+import { Grid, Paper, Typography, makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles((them) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  container: {
+    paddingTop: theme.spacing(5),
+    paddingBottom: theme.spacing(4),
+    justifyContent: "center",
+    display: "flex",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    display: "flex",
+    flexDirection: "column",
+  },
+}));
 
 export default function CohortAssignment() {
   const classes = useStyles();
@@ -11,7 +28,13 @@ export default function CohortAssignment() {
   return (
     <Fragment>
       <Grid container className={classes.root} spacing={1}>
-        <Typography>Hello from Cohort Assignment</Typography>
+        <Grid container className={classes.container} spacing={10}>
+          <Grid item xs={10}>
+            <Paper className={classes.paper}>
+              <Typography variant="h4">Cohort Assignments</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
       </Grid>
     </Fragment>
   );
